188f0e645f25b39482f7b6405004385957062ee1
mikitakalinovsky <mikitakalinovsky@gmail.com>
2021-06-09 23:01:04 +0300

    bug with width

--- lib/screens/uzi_screen.dart
+++ lib/screens/uzi_screen.dart
@@ -1,9 +1,44 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class UziScreen extends StatelessWidget{
+class UziScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   
-  }
+    return Container(
+      child: Row(
+        children: [
+          Column(
+            children: [
+              Container(
+                // width: MediaQuery.of(context).size.width*0.5,
+                child: TextField(
+                ),
+              ),
+              Container(
+                child: TextField(
+                ),
+              ),
+              Container(
+                child: TextField(
+                ),
+              ),
+              Container(
+              ),
+              Container(
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              Center(
+                child: Container(
 
+                    color: Colors.green, child: Text("x0,52")),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
 }
