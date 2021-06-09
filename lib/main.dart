5ff1cd0a46bb6bad2544e6c16e3a5bea6102a064
mikitakalinovsky <mikitakalinovsky@gmail.com>
2021-06-09 21:58:10 +0300

    Create bottom navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,61 @@
-import 'package:fclinick_helper/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(HomeScreen());
 }
 
+class HomeScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> _elements = [
+      Text("1"),
+      Text("2"),
+      Text("3"),
+    ];
+
+    void changeTab(int index) {
+      setState(() {
+        _currentIndex = index;
+      });
+    }
+
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.purple),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.purple,
+          title: Text("FClinic Helper"),
+        ),
+        body: Center(
+          child: _elements.elementAt(_currentIndex),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.zoom_in),
+              label: "Узи",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today_outlined),
+              label: "Узи",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.attach_money),
+              label: "Цена",
+            ),
+          ],
+          currentIndex: _currentIndex,
+          onTap: changeTab,
+        ),
+      ),
+    );
+  }
+}
